8d725f3fed1cd553f0a048c5c559c15f94b76e86
hossam ali <dev.hossam.ali@gmail.com>
2019-05-20 21:36:29 +0200

    Extension Update

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const isValidToken = (rootURI, token, userId) => {
-    const tokenQuery = '?access_token=' + token;
+    const tokenQuery = "?access_token=" + token;
     return axios.get(`${rootURI}/api/users/${userId + tokenQuery}`).catch((err) => {
-        console.log(err)
+        console.log(err);
     });
 };
 
 const updateState = (instance, app, props, global) => {
-	console.log("TCL: updateState -> instance", instance)
     return instance.backendApi.getProperties().then((state) => {
         props.forEach((prop) => state[prop.key] = prop.value);
         instance.backendApi.setProperties(state);
@@ -16,7 +15,7 @@ const updateState = (instance, app, props, global) => {
         global.isPersonalMode((reply) => {
             const isPersonalMode = reply.qReturn;
             if (isPersonalMode) {
-                console.log('Personal Mode!');
+                console.log("Personal Mode!");
                 app.doSave();
             }
         });
--- src/properties.js
+++ src/properties.js
@@ -3,91 +3,90 @@ import axios from "axios";
 import { isValidToken } from "@/auth";
 
 import {
-	setRefValue
+    setRefValue
 } from "@/utils";
 
 
 export const properties = () => {
-	// *****************************************************************************
-	// Settings section
-	// *****************************************************************************
-	var appearanceSection = {
-		uses: "settings",
-		items: {
-			defaultmode: {
-				type: "boolean",
-				label: "Switch Settings",
-				ref: "DefaultMode",
-				component: "switch",
-				options: [{
-					value: true,
-					label: "Voice Control Switch Enabled"
-				}, {
-					value: false,
-					label: "Voice Control Always On"
-				}],
-				defaultValue: true
-			},
-			notificationmode: {
-				type: "boolean",
-				label: "Notifications",
-				ref: "NotificationMode",
-				component: "switch",
-				options: [{
-					value: true,
-					label: "Enabled"
-				}, {
-					value: false,
-					label: "Disabled"
-				}],
-				defaultValue: true
-			},
-			contextmode: {
-				type: "string",
-				label: "Selection Context",
-				component: "radiobuttons",
-				ref: "ContextMode",
-				options: [{
-					value: "CurrentSelections",
-					label: "Current Selections"
-				}, {
-					value: "Cleared",
-					label: "Cleared"
-				}, {
-					value: "LockedFieldsOnly",
-					label: "Locked Fields Only"
-				}],
-				defaultValue: "CurrentSelections"
-			},
-			debugmode: {
-				type: "boolean",
-				label: "Debug Mode",
-				ref: "DebugMode",
-				defaultValue: true
-			}
-		}
-	};
+    // *****************************************************************************
+    // Settings section
+    // *****************************************************************************
+    var appearanceSection = {
+        uses: "settings",
+        items: {
+            defaultmode: {
+                type: "boolean",
+                label: "Switch Settings",
+                ref: "DefaultMode",
+                component: "switch",
+                options: [{
+                    value: true,
+                    label: "Voice Control Switch Enabled"
+                }, {
+                    value: false,
+                    label: "Voice Control Always On"
+                }],
+                defaultValue: true
+            },
+            notificationmode: {
+                type: "boolean",
+                label: "Notifications",
+                ref: "NotificationMode",
+                component: "switch",
+                options: [{
+                    value: true,
+                    label: "Enabled"
+                }, {
+                    value: false,
+                    label: "Disabled"
+                }],
+                defaultValue: true
+            },
+            contextmode: {
+                type: "string",
+                label: "Selection Context",
+                component: "radiobuttons",
+                ref: "ContextMode",
+                options: [{
+                    value: "CurrentSelections",
+                    label: "Current Selections"
+                }, {
+                    value: "Cleared",
+                    label: "Cleared"
+                }, {
+                    value: "LockedFieldsOnly",
+                    label: "Locked Fields Only"
+                }],
+                defaultValue: "CurrentSelections"
+            },
+            debugmode: {
+                type: "boolean",
+                label: "Debug Mode",
+                ref: "DebugMode",
+                defaultValue: true
+            }
+        }
+    };
 
-	// *****************************************************************************
-	// Main properties panel definition
-	// Only what is defined here is returned from properties.js
-	// *****************************************************************************
-	return {
-
-		type: "items",
-		component: "accordion",
-		items: {
-			Settings: appearanceSection,
-			about: {
-				label: "About",
-				ref: "auth",
-				items: {
-					header: {
-						type: "string",
-						label: "Header Holder",
-						ref: "NAHEADER",
-						component: {
-							template: `
+    // *****************************************************************************
+    // Main properties panel definition
+    // Only what is defined here is returned from properties.js
+    // *****************************************************************************
+    return {
+        type: "items",
+        component: "accordion",
+        items: {
+            Settings: appearanceSection,
+            about: {
+                label: "About",
+                ref: "auth",
+                items: {
+                    header: {
+                        type: "string",
+                        label: "Header Holder",
+                        ref: "NAHEADER",
+                        component: {
+                            template: `
                             <div class="pp-component">
                                 <img src="https://s3-eu-west-1.amazonaws.com/calltoaction-public-assets/w0w-toolkit-logo.svg"
                                 style="width: 70%;"/>
@@ -102,36 +101,36 @@ export const properties = () => {
                                 </div>
                             </div>
                             `}
-					},
-					doclink: {
-						type: "button",
-						label: "Documentation",
-						ref: "NADOCLINK"
-					},
-					environment: {
-						type: "string",
-						label: "Environment",
-						ref: "environment",
-						defaultValue: "https://app.webofwork.com"
-					},
-					useremail: {
-						type: "string",
-						label: "User Email",
-						ref: "userEmail",
-						show: false
-					},
-					userid: {
-						type: "string",
-						label: "User Id",
-						ref: "userId",
-						show: false
-					},
-					accesstoken: {
-						type: "string",
-						label: "Writeback Service",
-						ref: "accessToken",
-						component: {
-							template: `
+                    },
+                    doclink: {
+                        type: "button",
+                        label: "Documentation",
+                        ref: "NADOCLINK"
+                    },
+                    environment: {
+                        type: "string",
+                        label: "Environment",
+                        ref: "environment",
+                        defaultValue: "https://app.webofwork.com"
+                    },
+                    useremail: {
+                        type: "string",
+                        label: "User Email",
+                        ref: "userEmail",
+                        show: false
+                    },
+                    userid: {
+                        type: "string",
+                        label: "User Id",
+                        ref: "userId",
+                        show: false
+                    },
+                    accesstoken: {
+                        type: "string",
+                        label: "Writeback Service",
+                        ref: "accessToken",
+                        component: {
+                            template: `
                             <div id="authPanel" class="auth_panel">
                                 <div id="authPanelContainer">
                                     <div id="authForm" ng-if="!isLoggedIn">
@@ -170,135 +169,109 @@ export const properties = () => {
                                 <button ng-click="logout()" id="logoutBtn" class="lui-button  lui-button--block">Logout</button>
                             </div>
                             `,
-							controller: ["$scope", "$element", function ($scope, $element) {
-								console.log("TCL: properties -> $element", $element);
-								console.log("TCL: $scope -> c", $scope);
+                            controller: ["$scope", "$element", function ($scope, $element) {
+                                console.log("TCL: properties -> $element", $element);
+                                console.log("TCL: $scope -> c", $scope);
 
-								let data = $scope.$parent.data;
+                                let data = $scope.$parent.data;
 
-								console.log("extension data", data);
-
-
-								if (data.accessToken != "") {
-
-									isValidToken(data.environment, data.accessToken, data.userId).then(function (r) {
-										$scope.isLoggedIn = true;
-										console.log("Logged in.");
-									}).catch((err) => {
-										console.log(err);
-										$scope.isLoggedIn = false;
-										console.log("Token exists but not valid.");
-
-									});
-
-								} else {
-
-									$scope.isLoggedIn = false;
+                                console.log("extension data", data);
 
 
-								}
+                                if (data.accessToken != "") {
 
-								//$scope.URL = 'https://' + data.rootURI + '/api/writebacks/' + (data.writebackId || '{Id}');
+                                    isValidToken(data.environment, data.accessToken, data.userId).then(function (r) {
+                                        $scope.isLoggedIn = true;
+                                        console.log("Logged in.");
+                                    }).catch((err) => {
+                                        console.log(err);
+                                        $scope.isLoggedIn = false;
+                                        console.log("Token exists but not valid.");
 
+                                    });
 
-								$scope.login = function (data) {
+                                } else {
 
-									let email = $("#authEmail").val();
-									let password = $("#authPassword").val();
-									if (!email.trim() || !password.trim()) {
-										return alert("Invalid login form");
-									}
-
-									let authURL = `${$scope.data.environment}/api/users/login`;
-
-									axios.post(authURL, {
-										email,
-										password
-									}).then(function (res) {
-										console.log("response from server", res);
-
-										isValidToken($scope.data.environment, res.data.id, res.data.userId).then(function (r) {
-
-											// Updating the layout and save
-											console.log("Response from token check", res);
-
-											setRefValue($scope.data, "accessToken", res.data.id);
-											setRefValue($scope.data, "userId", res.data.userId);
-											setRefValue($scope.data, "userEmail", $("#authEmail").val());
+                                    $scope.isLoggedIn = false;
 
 
-											$scope.isLoggedIn = true;
-											$scope.errormessage = null;
+                                }
 
-											//  "function" == typeof c.definition.change && c.definition.change(c.data, c.args.handler);
-											$scope.$emit("saveProperties");
-
-											//app.doSave();
-											console.log("Token Saved.");
-										});
-
-									})
-										.catch((err) => {
-											console.log(err);
-											setRefValue($scope.data, "accessToken", "");
-
-											$scope.isLoggedIn = false;
-											console.log("Token Invalid.");
-											$scope.errormessage = "Token is invalid. Please login.";
-
-											//  "function" == typeof c.definition.change && c.definition.change(c.data, c.args.handler);
-											$scope.$emit("saveProperties");
+                                //$scope.URL = 'https://' + data.rootURI + '/api/writebacks/' + (data.writebackId || '{Id}');
 
 
-										});
-								};
+                                $scope.login = function (data) {
 
-								$scope.logout = function () {
-									setRefValue($scope.data, "accessToken", "");
-									setRefValue($scope.data, "userId", "");
-									setRefValue($scope.data, "userEmail", "");
-									$scope.isLoggedIn = false;
+                                    let email = $("#authEmail").val();
+                                    let password = $("#authPassword").val();
+                                    if (!email.trim() || !password.trim()) {
+                                        return alert("Invalid login form");
+                                    }
 
-									//Timeout token server side. DO THIS
+                                    let authURL = `${$scope.data.environment}/api/users/login`;
+
+                                    axios.post(authURL, {
+                                        email,
+                                        password
+                                    }).then(function (res) {
+                                        console.log("response from server", res);
+
+                                        isValidToken($scope.data.environment, res.data.id, res.data.userId).then(function (r) {
+
+                                            // Updating the layout and save
+                                            console.log("Response from token check", res);
+
+                                            setRefValue($scope.data, "accessToken", res.data.id);
+                                            setRefValue($scope.data, "userId", res.data.userId);
+                                            setRefValue($scope.data, "userEmail", $("#authEmail").val());
 
 
-									//  "function" == typeof c.definition.change && c.definition.change(c.data, c.args.handler);
-									$scope.$emit("saveProperties");
-									console.log("Token Cleared.");
+                                            $scope.isLoggedIn = true;
+                                            $scope.errormessage = null;
+
+                                            //  "function" == typeof c.definition.change && c.definition.change(c.data, c.args.handler);
+                                            $scope.$emit("saveProperties");
+
+                                            //app.doSave();
+                                            console.log("Token Saved.");
+                                        });
+
+                                    })
+                                        .catch((err) => {
+                                            console.log(err);
+                                            setRefValue($scope.data, "accessToken", "");
+
+                                            $scope.isLoggedIn = false;
+                                            console.log("Token Invalid.");
+                                            $scope.errormessage = "Token is invalid. Please login.";
+
+                                            //  "function" == typeof c.definition.change && c.definition.change(c.data, c.args.handler);
+                                            $scope.$emit("saveProperties");
 
 
-								};
-							}]
-						}
-					}
-				}
-			}
-		}
-	};
+                                        });
+                                };
+
+                                $scope.logout = function () {
+                                    setRefValue($scope.data, "accessToken", "");
+                                    setRefValue($scope.data, "userId", "");
+                                    setRefValue($scope.data, "userEmail", "");
+                                    $scope.isLoggedIn = false;
+
+                                    //Timeout token server side. DO THIS
+
+
+                                    //  "function" == typeof c.definition.change && c.definition.change(c.data, c.args.handler);
+                                    $scope.$emit("saveProperties");
+                                    console.log("Token Cleared.");
+
+
+                                };
+                            }]
+                        }
+                    }
+                }
+            }
+        }
+    };
 };
-
-
-
-/*
-			notificationposition : {
-				type: "string",
-				label: "Position",
-				component: "dropdown",
-				ref: "notificationposition",
-				options: [
-				{
-					value: "below",
-					label: "Below"
-				},{
-					value: "left",
-					label: "Left"
-				},{
-					value: "right",
-					label: "Right"
-				},{
-					value: "above",
-					label: "Above"
-				}],
-				defaultValue: "below"
-			},
-			*/
